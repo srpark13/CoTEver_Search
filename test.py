99ce0be2f7ff597abf27876c9b151d1e10cbb9c8
srpark13 <srpark13@yonsei.ac.kr>
2022-11-17 11:33:45 +0900

    asyncronized

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-from search import search
+from v2 import search
 import os
 import json
-with open("./tmp_input.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-tmp =search(data)
-with open(os.path.join('./', 'output_example.json'),'w',encoding="utf-8") as f:
-    f.write(tmp)
+def main():
+    with open("./tmp_input.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    tmp = search(data)
+    with open(os.path.join('./', 'output_example.json'),'w',encoding="utf-8") as f:
+        f.write(tmp)
+main()
