b400e5b88b213fae2ae08b8358e4db81d82c6bd2
Yul Jang <blaze@yonsei.ac.kr>
2022-11-17 12:22:42 +0900

    Change vanilla HTTP to asynchronous HTTP

--- search.py
+++ search.py
@@ -6,9 +6,14 @@ import json
 import re
 from readability import Document
 import os
+import aiohttp
+import asyncio
+import time
+
 url = 'https://www.google.com/search?q='
-google_search_api = "AIzaSyAB5Jaqdh98MZ8aMv9bBxdMynMhJGRBfGQ"
-google_engine_id = "74f117132b62c4715"
+# google_search_api = "AIzaSyAB5Jaqdh98MZ8aMv9bBxdMynMhJGRBfGQ"
+google_search_api = "AIzaSyCGjN1g_Zf3iKLACjHyqKdH4H48MerwwKw"
+google_engine_id = "90131cbef1ea540b2"
 header = { 
 'User-Agent' : ('Mozilla/5.0 (Windows NT 10.0;Win64; x64)\
 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98\
@@ -20,49 +25,90 @@ def google_search(search_term, api_key, cse_id, **kwargs):
     res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
     return res
 
+
 def html_parser(rough_html):
   parse_re = re.compile('<([^>]+)>|\t|\n|\r|\([^)]*\)|\[[^)]*\]')
   parsed_content = re.sub(parse_re, '', rough_html)
   return parsed_content
 
-def search(q):
+
+async def get(url, session):
+    try:
+        async with session.get(url=url, timeout=3) as response:
+            resp = await response.text()
+            print("Successfully got url {} with resp of length {}.".format(url, len(resp)))
+            return (url, resp)
+    except Exception as e:
+        print("Unable to get url {} due to {}.".format(url, e.__class__))
+        return (url, None)
+
+
+async def main_get(urls):
+    async with aiohttp.ClientSession() as session:
+        ret = await asyncio.gather(*[get(url, session) for url in urls])
+    print("Finalized all. Return is a list of len {} outputs.".format(len(ret)))
+    return ret
+
+
+async def search(q):
     sub_questions = q['explanation']
-    res={}
-    res['questions'] = []
     for i in sub_questions:
-        
         tmptmp_res = {}
         tmptmp_res[str(i)] = []
         search_result = google_search(sub_questions[str(i)]['sub_question'], google_search_api, google_engine_id)
         items = search_result['items']
-        title = []
-        url = []
-        content = []
+        url = {}
         
-        idx=0
-        cnt=0
-        while(cnt<5):
-            if(items[idx]['link'][-3:]!="pdf"):
-                response = requests.get(items[idx]['link'], headers = header)
-                doc = response.text
-                docu = Document(doc)
-                docu = docu.summary()
-                docu = html_parser(docu)
-                docu = docu.strip()
-                content.append(docu)
-                title.append(items[idx]['title'])
-                url.append(items[idx]['link'])
-                cnt+=1
-                idx+=1
-            else:
-                idx+=1
+        for item in items:
+            url[item['link']] = {}
+            url[item['link']]['title'] = item['title']
 
-        for k in range(0,5):
+        ret = await main_get(list(url.keys()))
+
+        for response in ret:
+            if response[1] != None:
+                doc = Document(response[1]).summary()
+                doc = html_parser(doc).strip()
+                url[response[0]]['content'] = doc
+            else:
+                del url[response[0]]
+            
+
+        cnt = 0
+        for key in url:
+            if cnt == 5:
+                break
             tmptmptmp_res = {}
-            tmptmptmp_res['url'] = url[k]
-            tmptmptmp_res['title'] = title[k]
-            tmptmptmp_res['content'] = content[k]
-            sub_questions[str(i)]['evidence_document'][str(k)]=tmptmptmp_res
+            tmptmptmp_res['url'] = key
+            tmptmptmp_res['title'] = url[key]['title']
+            tmptmptmp_res['content'] = url[key]['content']
+            sub_questions[str(i)]['evidence_document'][cnt]=tmptmptmp_res
+            cnt += 1
+
+        # idx=0
+        # cnt=0
+        # while(cnt<5):
+        #     if(items[idx]['link'][-3:]!="pdf"):
+        #         response = requests.get(items[idx]['link'], headers = header)
+        #         doc = response.text
+        #         docu = Document(doc)
+        #         docu = docu.summary()
+        #         docu = html_parser(docu)
+        #         docu = docu.strip()
+        #         content.append(docu)
+        #         title.append(items[idx]['title'])
+        #         url.append(items[idx]['link'])
+        #         cnt+=1
+        #         idx+=1
+        #     else:
+        #         idx+=1
+
+        # for k in range(0,5):
+        #     tmptmptmp_res = {}
+        #     tmptmptmp_res['url'] = url[k]
+        #     tmptmptmp_res['title'] = title[k]
+        #     tmptmptmp_res['content'] = content[k]
+        #     sub_questions[str(i)]['evidence_document'][str(k)]=tmptmptmp_res
        
     # json_file = json.dumps(q,ensure_ascii=False, indent=4)
     return q
